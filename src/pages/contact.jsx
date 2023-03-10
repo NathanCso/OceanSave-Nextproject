85c08035b95ee28ba0595a162796d7617abf330a
NathanCso <109382054+NathanCso@users.noreply.github.com>
2023-03-10 15:49:53 +0100

    up

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -5,7 +5,7 @@ import Hero from '../components/Hero'
 const contact = () => {
   return (
     <div>
-        <Hero heading='Contact' message='Si vous souhaiter contribuer a notre association merci de nous contacter sur cette page' />
+        <Hero heading='Contact' message='Si vous souhaiter contribuer à notre association merci de nous contactez sur cette page' />
         <Contact />
     </div>
   )
