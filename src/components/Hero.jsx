c04b42aaf9993c997279a0511f359868f1ae5cd8
NathanCso <109382054+NathanCso@users.noreply.github.com>
2023-03-10 15:39:33 +0100

    up

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = ({heading, message}) => {
       <div className='p-5 text-white z-[2] mt-[-10rem]'>
         <h2 className='text-5xl font-bold'>{heading}</h2>
         <p className='py-5 text-xl'>{message}</p>
-        <button className='px-8 py-2 border'>"Le climat change, peut-être que nous devrions faire de même"
+        <button className='px-8 py-2 border'>'Le climat change, peut-être que nous devrions faire de même'
 </button>
       </div>
     </div>
